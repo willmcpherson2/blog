584954194ccd580055335972aaa624718d73e30f
William McPherson <willmcpherson2@gmail.com>
2022-12-24 22:26:06 +1100

    Add intro to Tulip demo

--- client/src/TulipDemo.hs
+++ client/src/TulipDemo.hs
@@ -1,12 +1,14 @@
 module TulipDemo where
 
 import Data.Text (Text, pack, unpack)
+import Markup.M (m)
 import Reflex.Dom
 import Text.RawString.QQ
 import Tulip (getResult)
 
 tulip :: Widget x ()
 tulip = do
+  intro
   area <-
     textAreaElement $
       def
@@ -16,6 +18,50 @@ tulip = do
   let handle = pack . getResult . unpack
   output <- foldDyn (\source _ -> handle source) (handle tulipExample) inputEvent
   el "pre" $ elClass "code" "code-block" $ dynText output
+
+intro :: Widget x ()
+intro =
+  [m|
+
+#[
+Tulip Interpreter
+]
+
+p[
+@[[github.com/willmcpherson2/tulip][https://github.com/willmcpherson2/tulip]]
+]
+
+p[
+A function `|[[x y]]| is a parameter and a body.
+Multiple parameters `|[[x y z]]| are interpreted as `|[[x [y z]]]|.
+]
+
+p[
+Applications `[(f x)] let you apply values to functions.
+The result is the body of the function on the left with its parameter substituted by the value on the right.
+Multiple applications `[(f x y)] are interpreted as `[((f x) y)].
+]
+
+p[
+A variable `[x] refers to a previously defined value.
+They can be shadowed.
+]
+
+p[
+Holes `[_] can't be bound, and so can be used to ignore an argument.
+They can also be used to signify undefined values.
+]
+
+p[
+Definitions `[(x y)] can be recursive.
+They have the same syntax as applications, but are top-level only.
+]
+
+p[
+The entrypoint to a program is `[main].
+]
+
+|]
 
 tulipExample :: Text
 tulipExample =
