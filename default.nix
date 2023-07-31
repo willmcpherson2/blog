9e5497d2ae90e0324ff31616b48ea3c8f2f828cd
William McPherson <willmcpherson2@gmail.com>
2023-07-31 17:25:05 +1000

    Remove demos

--- default.nix
+++ default.nix
@@ -4,23 +4,12 @@ let
     (import
       (builtins.fetchTarball
         "https://github.com/willmcpherson2/nixture/archive/aa5a7df9d51f55823ccba0755ff3026598e1f905.tar.gz"))
-    ghc ghcjs optimiseHaskellJs compileNixture;
-  parss =
-    ghcjs.callCabal2nix
-      "parss"
-      (builtins.fetchTarball "https://github.com/willmcpherson2/parss/archive/4673e197e5036e370e53fffe962fcea72c28af62.tar.gz")
-      { };
-  two-hand =
-    ghcjs.callCabal2nix
-      "two-hand"
-      (builtins.fetchTarball "https://github.com/willmcpherson2/two-hand/archive/01f00f61bc5a7c4774f22a57dbc796867677ef5f.tar.gz")
-      { inherit parss; };
+    ghc compileNixture;
 in
 symlinkJoin {
   name = "willmcpherson2.com";
   paths = [
     (ghc.callCabal2nix "server" ./server { })
-    (optimiseHaskellJs (ghcjs.callCabal2nix "hs" ./client/hs { inherit two-hand; }))
     (compileNixture ./client)
   ];
 }
