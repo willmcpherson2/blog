d8d05289a84459fc7cb51ae58ad2578cfb4b7480
William McPherson <willmcpherson2@gmail.com>
2024-04-14 17:15:21 +1000

    Format nix

--- flake.nix
+++ flake.nix
@@ -5,25 +5,18 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       server = pkgs.static-web-server;
-      static = pkgs.runCommand
-        "build.sh"
-        {
-          buildInputs = [
-            pkgs.emacs29
-            (pkgs.writeShellScriptBin "build.sh" (builtins.readFile ./build.sh))
-          ];
-        }
-        ''
-          build.sh ${./init.el} ${./client} $out/static
-        '';
-    in
-      {
-        packages.x86_64-linux.default = pkgs.symlinkJoin {
-          name = "blog";
-          paths = [
-            server
-            static
-          ];
-        };
+      static = pkgs.runCommand "build.sh" {
+        buildInputs = [
+          pkgs.emacs29
+          (pkgs.writeShellScriptBin "build.sh" (builtins.readFile ./build.sh))
+        ];
+      } ''
+        build.sh ${./init.el} ${./client} $out/static
+      '';
+    in {
+      packages.x86_64-linux.default = pkgs.symlinkJoin {
+        name = "blog";
+        paths = [ server static ];
       };
+    };
 }
